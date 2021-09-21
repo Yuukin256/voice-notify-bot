cd7c87fbf4ac405eed7f4dd8c7305f16a11194d2
Yuukin256 <Yuukin256@gmail.com>
2021-09-21 23:55:34 +0900

    fix command.ts

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -1,7 +1,7 @@
-import { Guild, PrismaClient } from '.prisma/client';
 import { ApplicationCommandData, CommandInteraction } from 'discord.js';
+import { Channel, Guild, PrismaClient } from '.prisma/client';
 
 export interface Command {
   data: ApplicationCommandData;
-  exec: (command: CommandInteraction, dbGuild: Guild, prisma: PrismaClient) => unknown;
+  exec: (command: CommandInteraction, dbGuild: Guild & { channels: Channel[] }, prisma: PrismaClient) => unknown;
 }
