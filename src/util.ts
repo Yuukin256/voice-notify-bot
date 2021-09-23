744afee0a2de08b2fff620c8cefe34b94a5c5ab7
Yuukin256 <Yuukin256@gmail.com>
2021-09-23 22:23:34 +0900

    update util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,15 @@
+import { TextChannel, Role, GuildMember, EmbedFieldData } from 'discord.js';
+
 export const nonNullable = <T>(value: T): value is NonNullable<T> => value != null;
+
+export const makeMentionSettingEmbed = (
+  channel: TextChannel,
+  roles: (Role | null)[],
+  users: (GuildMember | null)[]
+): EmbedFieldData => {
+  const mentions = [...roles, ...users].filter(nonNullable);
+  return {
+    name: channel.name,
+    value: mentions.length ? mentions.join(' ') : 'メンションなし',
+  };
+};
