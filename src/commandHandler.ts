e7e571e37254d3ab119d2d2ea133b57a2a77498a
Yuukin256 <Yuukin256@gmail.com>
2021-09-23 23:29:18 +0900

    update command type

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,25 +1,30 @@
-import { PrismaClient } from '.prisma/client';
-import { Collection, CommandInteraction, Guild } from 'discord.js';
-import { Command } from './commands/command';
-import pingCommand from './commands/ping';
+import { PrismaClient } from '@prisma/client';
+import { ApplicationCommand, Collection, CommandInteraction, Guild } from 'discord.js';
+
+import { Command, CommandInteractionWithGuild } from './commands/command';
 import notifyChannelCommand from './commands/notifyChannel';
+import pingCommand from './commands/ping';
+import setCommand from './commands/set';
 
 export default class CommandHandler {
-  commands = new Collection<string, Command>([pingCommand, notifyChannelCommand].map((cmd) => [cmd.data.name, cmd]));
+  commands = new Collection<string, Command>(
+    [pingCommand, notifyChannelCommand, setCommand].map((cmd) => [cmd.data.name, cmd])
+  );
   prisma: PrismaClient;
 
   constructor(prisma: PrismaClient) {
     this.prisma = prisma;
   }
 
-  public setCommandsToGuild(guild: Guild) {
+  public setCommandsToGuild(guild: Guild): Promise<Collection<string, ApplicationCommand>> {
     return guild.commands.set(this.commands.map((cmd) => cmd.data));
   }
 
-  public async execCommand(command: CommandInteraction) {
+  public async execCommand(command: CommandInteraction): Promise<void> {
     if (!command.guildId || !command.guild) return;
 
     const exec = this.commands.get(command.commandName)?.exec;
+
     if (exec) {
       // データベースからサーバーの情報を取得 (なければレコードを作成)
       const dbGuild = await this.prisma.guild.upsert({
@@ -32,8 +37,13 @@ export default class CommandHandler {
           channels: true,
         },
       });
-      exec(command, dbGuild, this.prisma);
-    } else {
+
+      // コマンド実行
+      // 型ガードしてる
+      exec(command as CommandInteractionWithGuild, dbGuild, this.prisma);
+    }
+    // コマンドを実行する関数がないとき
+    else {
       command.reply({ content: 'エラーが発生しました。' });
     }
   }
--- src/commands/command.ts
+++ src/commands/command.ts
@@ -1,7 +1,14 @@
 import { ApplicationCommandData, CommandInteraction } from 'discord.js';
+
 import { Channel, Guild, PrismaClient } from '.prisma/client';
+
+export type CommandInteractionWithGuild = CommandInteraction & { guild: Guild; guildId: string };
 
 export interface Command {
   data: ApplicationCommandData;
-  exec: (command: CommandInteraction, dbGuild: Guild & { channels: Channel[] }, prisma: PrismaClient) => unknown;
+  exec: (
+    command: CommandInteractionWithGuild,
+    dbGuild: Guild & { channels: Channel[] },
+    prisma: PrismaClient
+  ) => unknown;
 }
